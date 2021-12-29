5fdfaf60a3be259c3c582bbafa96068f81184f24
WinayGupta777 <vinay.gupta8101@gmail.com>
2021-12-29 15:14:06 +0530

    Middle img & condition applied

--- src/components/middle.js
+++ src/components/middle.js
@@ -6,7 +6,10 @@ class Middle extends React.Component{
     }
     render(){
         return(
-            <div className="svg"></div>
+            <div className="svg">
+                <img src="https://cdn-icons.flaticon.com/png/512/3222/premium/3222807.png?token=exp=1640771541~hmac=6d09cb047c9e6f51a9897bc6655aef1c"></img>
+                <span className="condition">Sunny</span>
+            </div>
         )
     }
 }
