a8d2c7460831f65f02dace4fab44181785b0df0f
WinayGupta777 <vinay.gupta8101@gmail.com>
2021-12-29 23:56:08 +0530

    CLick & get time DOne

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import axios from "axios";
 import Header from "./header";
 import Middle from "./middle";
 import Footer from "./footer";
@@ -8,13 +8,25 @@ import styles from './card.css';
 class Card extends React.Component{
     constructor(props){
         super(props)
+        this.state={
+            time:""
+        }
+    }
+    getTime=()=>{
+        axios.get("http://worldtimeapi.org/api/timezone/Asia/Kolkata")
+        .then((result)=>{
+            const t = result.data.datetime;
+            //console.log(t.split('T')[1].substr(0,5))
+            this.setState({time:t.split('T')[1].substr(0,5)})
+        })
     }
     render(){
         return(
             <div className="cardlayout">
-                <Header  place="Dubai" time="19:52"></Header>
-                <Middle  img="cloudy"></Middle>
+                <Header  place="Dubai" time={this.state.time}></Header>
+                <Middle  img="Cloudy"></Middle>
                 <Footer  wind="2" humi="23" temp="32"></Footer>
+                <button onClick={this.getTime}>Gettime</button>
             </div>
         )
     }
